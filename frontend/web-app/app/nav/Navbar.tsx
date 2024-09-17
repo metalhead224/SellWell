b5b9d74eae70cee4db826dc1f9a49cc7c057b02b
metalhead224 <suman.osti401@gmail.com>
2024-09-17 13:49:03 -0400

    authJs added to the client

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import React from 'react'
 import Search from './Search';
 import Logo from './Logo';
+import LoginButton from './LoginButton';
 
 
 export default function Navbar() {
@@ -8,7 +11,7 @@ export default function Navbar() {
     <header className='sticky top-0 z-50 flex justify-between p-5 font-gray-800 shadow-md items-center'>
         <Logo />
         <Search />
-        <div>Login</div>
+        <LoginButton />
     </header>
   )
 }
--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -36,10 +36,11 @@ public static class Config
                 ClientSecrets = {new Secret("secret".Sha256())},
                 AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
                 RequirePkce = false,
-                RedirectUris = {"http://localhost:3000/auth/api/callback/id-server"},
+                RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
                 AllowOfflineAccess = true,
                 AllowedScopes = {"openid", "profile", "auctionApp"},
-                AccessTokenLifetime = 3600*24*30
+                AccessTokenLifetime = 3600*24*30,
+                AlwaysIncludeUserClaimsInIdToken = true
             }
         };
 }
