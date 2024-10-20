781f6f60952fb28231c59f15dd318d7002a15953
metalhead224 <suman.osti401@gmail.com>
2024-10-19 22:13:17 -0400

    added bid service

--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -1,3 +1,5 @@
+using BiddingService.Consumers;
+using BiddingService.RequestHelpers;
 using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MongoDB.Driver;
@@ -11,6 +13,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddMassTransit(x => 
 {
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
 
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("bids", false));
 
@@ -33,6 +36,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.ValidateAudience = false;
         options.TokenValidationParameters.NameClaimType = "username";
     });
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
